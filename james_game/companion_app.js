f4db0ee9b1a349e78a243160b10a24d4a3a429c6
James <james@james.net>
2026-04-17 09:25:27 +0200

    Added companion app for James Game

---
+++ james_game/companion_app.js
@@ -0,0 +1,16 @@
+// Import necessary libraries
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+// Create a companion app for James Game
+const CompanionApp = () => {
+  return (
+    <div>
+      <h1>James Game Companion App</h1>
+      <p>This app provides additional features and tools for James Game.</p>
+    </div>
+  );
+};
+
+// Render the companion app
+ReactDOM.render(<CompanionApp />, document.getElementById('companion-app'));
